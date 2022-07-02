c8a3a2963aea8c4a7fdccc87d96c44a82b1d36c8
Matthew Mifsud <97695752+mifsudmatthew@users.noreply.github.com>
2022-07-02 18:38:41 +0200

    organized
    ready

--- src/mainFrame.java
+++ src/mainFrame.java
@@ -4,7 +4,6 @@ import java.awt.*;
 public class mainFrame extends JFrame {
 
     mainFrame(){
-            // test for update..
         Color backgroundColor = new Color(144,238,144);
         this.setSize(420,420);
         this.setTitle("Data Organizer");
